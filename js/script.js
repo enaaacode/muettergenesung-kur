ad29b7ad191c56a62a60348735c4b483d9d94ff0
enaaacode <152639159+enaaacode@users.noreply.github.com>
2025-06-04 13:33:30 +0200

    edit mobile nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-const password = prompt("Bitte geben Sie das Passwort ein:");
-if (password !== "muttergenesungWebsiteNeu2025") {
-  document.body.innerHTML = "Access denied.";
-}
+// ESC schließt Menü (mobile)
+document.addEventListener("keydown", function (e) {
+  const toggle = document.getElementById("nav-toggle");
+  if (e.key === "Escape" && toggle.checked) {
+    toggle.checked = false;
+  }
+});
+
+// Beim Klick auf Link schließt das Menü (mobile)
+document.querySelectorAll(".mobile-nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("nav-toggle").checked = false;
+  });
+});
+
+const navToggle = document.getElementById("nav-toggle");
+
+navToggle.addEventListener("change", function () {
+  document.body.classList.toggle("menu-open", this.checked);
+});
